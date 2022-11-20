c459571075e3dcf82ff0a1806a33f23ae2fae333
keithythefrog <Keithythefrog@gmail.com>
2022-11-20 03:05:59 -0500

    Added admin page fixed footer

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
-console.log("Connected")
+let pool;
+
+let parentPoolList = document.querySelectorAll("#parentDropdown ul li");
+for (let i = 0; i < parentPoolList.length; i++) {
+    parentPoolList[i].addEventListener('click', listItem);
+}
+
+
+function listItem(e) {
+    for (let j = 0; j < parentPoolList.length; j++) {
+        parentPoolList[j].classList.remove("newBorder")
+    }
+
+    e.target.classList.add("newBorder")
+    pool = e.target.id;
+    console.log(pool);
+}
+
+const confirmParentPoolb = document.querySelector("#confirmParent");
+
+const FireEmailSend = () => {
+    if (pool === undefined) {
+        alert("Please selected a pool");
+        return;
+    }
+    console.log("Selected pool:" + pool);
+    //fire database query
+}
+
+confirmParentPoolb.addEventListener('click', FireEmailSend);
+
+
+
+
+
+

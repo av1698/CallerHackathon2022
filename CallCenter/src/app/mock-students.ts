bccdcfbcd88ada45be25b78f13745cdc379a6960
Angel Vasquez <av1698@rit.edu>
2022-11-20 10:47:17 -0500

    reformatted format and replaced footer and header with imgs

--- CallCenter/src/app/mock-students.ts
+++ CallCenter/src/app/mock-students.ts
@@ -6,91 +6,91 @@ export const STUDENTS: Student[] = [
     "ID": 1,
     "studentName": "Boycey Macvain",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 2,
     "studentName": "Liana Frill",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "aliquam sit amet diam in magna bibendum imperdiet nullam orci pede",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 3,
     "studentName": "Mohammed Gianninotti",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 4,
     "studentName": "Umberto Sangwin",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "nunc commodo placerat praesent blandit nam nulla integer pede justo",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 5,
     "studentName": "Godfrey Nutting",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis aliquam convallis nunc proin at turpis a pede",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 6,
     "studentName": "Raquela Tynemouth",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 7,
     "studentName": "Laural Evered",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 8,
     "studentName": "Tad MacCarroll",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "rhoncus sed vestibulum sit amet cursus id turpis integer aliquet",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 9,
     "studentName": "Sukey Joffe",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 10,
     "studentName": "Kania Le Noury",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 11,
     "studentName": "Auria Stapylton",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 12,
     "studentName": "Flora Whitlaw",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus et",
+    "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   },
   {
     "ID": 13,
     "studentName": "Quandale Dingle",
     "videoLink": "https://youtu.be/Ooeg_SaFfWc",
-    "about": "help",
+    "about": "help, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut enim blandit volutpat. Fermentum posuere urna nec tincidunt praesent. Quam lacus suspendisse faucibus interdum posuere lorem. Donec enim diam vulputate ut pharetra sit. Ultrices tincidunt arcu non sodales neque sodales ut etiam. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et ultrices neque ornare aenean. Mattis ullamcorper velit sed ullamcorper. ",
     "imageLink": "https://thispersondoesnotexist.com/image"
   }
 ]
